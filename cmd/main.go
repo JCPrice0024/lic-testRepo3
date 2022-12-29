6cc679cd13c1a6b8c4bc77320c67f3cd4093b9b4
JCPrice0024 <jcprice0024@gmail.com>
2022-12-29 16:45:23 -0500

    Test

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	lictestrepo2 "github.com/JCPrice0024/lic-testRepo2"
+	lictestrepo4 "github.com/JCPrice0024/lic-testRepo4"
+)
+
+func main() {
+	fmt.Println(lictestrepo2.WelcomeUser("JCPrice0024"))
+	fmt.Println(lictestrepo4.Addition(124, 12))
 }
--- subtraction.go
+++ subtraction.go
@@ -1,5 +1,8 @@
 package lictestrepo3
 
+import lictestrepo1 "github.com/JCPrice0024/lic-testRepo1"
+
 func Subtraction(x, y int) int {
+	lictestrepo1.HelloUser("JCPRICE")
 	return x - y
 }
